9ae598b2f68f529d7ad0b58921b67e528f736f92
Mark Moyes <mark.moyes@cbc.ca>
2015-04-29 07:54:16 -0700

    code add

---
+++ src/toastr-ui.js
@@ -0,0 +1,23 @@
+// Copyright (c) CBC/Radio-Canada. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+define(['text!./toastr.html', 'toastr'],
+    function(template, toastr) {
+        'use strict';
+
+        var ToastrViewModel = function() {
+            toastr.options = {
+                //timeOut: -1, // When testing, it might be useful to prevent them from disappearing
+                target: 'toastr > .container'
+            };
+        };
+
+        return {
+            viewModel: {
+                createViewModel: function(settings, componentInfo) {
+                    return new ToastrViewModel(settings, componentInfo);
+                }
+            },
+            template: template
+        };
+    });
